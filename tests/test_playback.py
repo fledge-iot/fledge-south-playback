c3f1d3caeccb9e766a80c1111c0751b2a792cbe8
Mohit Tomar <m.singh19994@gmail.com>
2022-10-12 19:46:31 +0530

    Addedd Unit Tests
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

---
+++ tests/test_playback.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# FLEDGE_BEGIN
+# See: http://fledge-iot.readthedocs.io/
+# FLEDGE_END
+
+from unittest.mock import patch
+import pytest
+
+from python.fledge.plugins.south.playback import playback
+
+
+
+def test_plugin_contract():
+    # Evaluates if the plugin has all the required methods
+    assert callable(getattr(playback, 'plugin_info'))
+    assert callable(getattr(playback, 'plugin_init'))
+    assert callable(getattr(playback, 'plugin_start'))
+    assert callable(getattr(playback, 'plugin_shutdown'))
+    assert callable(getattr(playback, 'plugin_reconfigure'))
